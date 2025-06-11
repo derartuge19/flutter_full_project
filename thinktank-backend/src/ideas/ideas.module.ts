fbc9d501e91055f3fafa976036ef6f968f2b1dbb
Hasset Dejene <162673489+hazmute@users.noreply.github.com>
2025-06-11 19:44:00 +0300

    Update ideas.module.ts

--- thinktank-backend/src/ideas/ideas.module.ts
+++ thinktank-backend/src/ideas/ideas.module.ts
@@ -3,11 +3,11 @@ import { IdeasService } from './ideas.service';
 import { IdeasController } from './ideas.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Idea } from './entities/ideas.entity'; // Assuming you have an Idea entity
-import { UsersModule } from '../users/users.module'; // If IdeasService needs to use UsersService, import UsersModule
+import { UsersModule } from '../users/users.module'; 
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 @Module({
-  imports: [TypeOrmModule.forFeature([Idea]), UsersModule], // Import the Idea entity and any other necessary modules
+  imports: [TypeOrmModule.forFeature([Idea]), UsersModule], 
   providers: [IdeasService, JwtAuthGuard, RolesGuard],
   controllers: [IdeasController],
   exports: [IdeasService],
