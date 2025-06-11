3b92a519e46524498d8e3b5130a984b9205a23b9
Eyob Fikre Enquosealssie <167536555+TheCodingE-Baby@users.noreply.github.com>
2025-06-11 20:17:39 +0300

    Update idea_provider.dart

--- thinktank-frontend/flutter_thinktank/flutter_thinktank/thinktank_flutter/lib/features/ideas/presentation/providers/idea_provider.dart
+++ thinktank-frontend/flutter_thinktank/flutter_thinktank/thinktank_flutter/lib/features/ideas/presentation/providers/idea_provider.dart
@@ -3,4 +3,4 @@ import 'package:thinktank_flutter/features/ideas/domain/repositories/idea_reposi
 
 final ideaRepositoryProvider = Provider<IdeaRepository>((ref) {
   throw UnimplementedError('This provider should be overridden in tests');
-}); 
+});
