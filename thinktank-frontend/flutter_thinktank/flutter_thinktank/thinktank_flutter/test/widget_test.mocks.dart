19162da085ff18bfd1ce97ef278eea6724920369
Derartu Nigatu Tuge <167537446+derartuge19@users.noreply.github.com>
2025-06-11 19:56:37 +0300

    Update widget_test.mocks.dart

--- thinktank-frontend/flutter_thinktank/flutter_thinktank/thinktank_flutter/test/widget_test.mocks.dart
+++ thinktank-frontend/flutter_thinktank/flutter_thinktank/thinktank_flutter/test/widget_test.mocks.dart
@@ -17,19 +17,7 @@ import 'package:thinktank_flutter/features/ideas/domain/entities/idea.dart'
 import 'package:thinktank_flutter/features/ideas/domain/repositories/idea_repository.dart'
     as _i7;
 
-// ignore_for_file: type=lint
-// ignore_for_file: avoid_redundant_argument_values
-// ignore_for_file: avoid_setters_without_getters
-// ignore_for_file: comment_references
-// ignore_for_file: deprecated_member_use
-// ignore_for_file: deprecated_member_use_from_same_package
-// ignore_for_file: implementation_imports
-// ignore_for_file: invalid_use_of_visible_for_testing_member
-// ignore_for_file: must_be_immutable
-// ignore_for_file: prefer_const_constructors
-// ignore_for_file: unnecessary_parenthesis
-// ignore_for_file: camel_case_types
-// ignore_for_file: subtype_of_sealed_class
+
 
 class _FakeEither_0<L, R> extends _i1.SmartFake implements _i2.Either<L, R> {
   _FakeEither_0(
@@ -40,10 +28,6 @@ class _FakeEither_0<L, R> extends _i1.SmartFake implements _i2.Either<L, R> {
           parentInvocation,
         );
 }
-
-/// A class which mocks [AuthRepository].
-///
-/// See the documentation for Mockito's code generation for more information.
 class MockAuthRepository extends _i1.Mock implements _i3.AuthRepository {
   MockAuthRepository() {
     _i1.throwOnMissingStub(this);
